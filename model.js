e5e6b67e0b223131e71f04f0d04f19c8b0d0483d
Denise Namutebi <denisenamutebi@gmail.com>
2021-01-28 14:05:12 +0000

    Adding server.js and model.js

--- model.js
+++ model.js
@@ -1,1 +1,22 @@
+const {Model, DataTypes, Sequelize} = require('sequelize')
+const sequelize = new Sequelize('sqlite:./banking.db')
 
+class Account extends Model {}
+Account.init({
+    current_balance: DataTypes.FLOAT,
+    sub: DataTypes.STRING
+    
+}, {sequelize})
+
+class Friend extends Model {}
+Friend.init({
+    email: DataTypes.STRING,
+    site_address: DataTypes.STRING,
+    sub: DataTypes.STRING
+}, {sequelize})
+
+module.exports = {
+    Account,
+    Friend,
+    sequelize
+}
--- server.js
+++ server.js
@@ -1,1 +1,48 @@
+const express = require("express")
+const app = express()
+const {Account, Friend, sequelize} = require("./model")
+const Handlebars = require('handlebars')
+const expressHandlebars = require("express-handlebars")
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const { auth } = require('express-openid-connect')
+const { requiresAuth } = require('express-openid-connect')
 
+if(process.env.NODE_ENV !== "production"){
+  require('dotenv').config()
+}
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_DOMAIN
+};
+
+const handlebars = expressHandlebars({
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+
+app.use(express.json())
+app.use(auth(config))
+app.use(express.static('public'))
+app.engine('handlebars', handlebars)
+app.set('view engine', 'handlebars')
+
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get("/profile", requiresAuth(), (req, res) => {
+   const person = req.oidc.user
+    res.render("profile", { person });
+  });
+
+
+
+app.listen(3000, () => {
+    sequelize.sync()
+    .then(console.log("Server is ready"))
+})
